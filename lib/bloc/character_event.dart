3b2fca1350b3b0a6dc4ff90cc7438668628ac7ac
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-03-06 15:12:25 +0600

    Finished bloc part :crocodile:

---
+++ lib/bloc/character_event.dart
@@ -0,0 +1,11 @@
+// This file responsible for events
+
+part of 'character_bloc.dart';
+
+@freezed
+class CharacterEvent with _$CharacterEvent {
+  const factory  CharacterEvent.fetch({
+    required String name,
+    required int page, 
+  }) = CharacterEventFetch;
+}
