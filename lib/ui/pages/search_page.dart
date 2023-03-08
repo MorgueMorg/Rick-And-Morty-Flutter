a962dfc4aea5e08373f56e799a4c9937e74fdf78
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-03-08 20:00:52 +0600

    Updated search_page :rocket:

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rick_and_morty/bloc/character_bloc.dart';
+import 'package:rick_and_morty/data/models/character.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -10,11 +11,18 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  late Character _currentCharacter;
+  List<Results> _currentResults = [];
+  int _currentPage = 1;
+  String _currentSearchStr = '';
+
   @override
   void initState() {
-    context
-        .read<CharacterBloc>()
-        .add(const CharacterEvent.fetch(name: '', page: 1));
+    if (_currentResults.isEmpty) {
+      context
+          .read<CharacterBloc>()
+          .add(const CharacterEvent.fetch(name: '', page: 1));
+    }
     super.initState();
   }
 
@@ -47,33 +55,65 @@ class _SearchPageState extends State<SearchPage> {
               hintStyle: const TextStyle(color: Colors.white),
             ),
             onChanged: (value) {
+              _currentPage = 1;
+              _currentResults = [];
+              _currentSearchStr = value;
+
               context.read<CharacterBloc>().add(
-                    CharacterEvent.fetch(name: value, page: 1),
+                    CharacterEvent.fetch(name: value, page: _currentPage),
                   );
             },
           ),
         ),
-        state.when(
-          loading: () {
-            return Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircularProgressIndicator(
-                    strokeWidth: 2,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text("Loading...")
-                ],
-              ),
-            );
-          },
-          loaded: (characterLoaded) => Text("${characterLoaded.info}"),
-          error: () => const Text("Nothing found..."),
+        Expanded(
+          child: state.when(
+            loading: () {
+              return Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(
+                      strokeWidth: 2,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Loading...")
+                  ],
+                ),
+              );
+            },
+            loaded: (characterLoaded) {
+              _currentCharacter = characterLoaded;
+              _currentResults = _currentCharacter.results;
+              return _currentResults.isNotEmpty
+                  ? _customListView(_currentResults)
+                  : const SizedBox();
+            },
+            error: () => const Text("Nothing found..."),
+          ),
         ),
       ],
+    );
+  }
+
+  Widget _customListView(List<Results> currentResults) {
+    return ListView.separated(
+      itemCount: currentResults.length,
+      itemBuilder: (context, index) {
+        final result = currentResults[index];
+        return Padding(
+          padding: const EdgeInsets.only(right: 16, left: 16, top: 3, bottom: 3),
+          child: ListTile(
+            title: Text(
+              result.name,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (_, index) => const SizedBox(height: 5),
+      shrinkWrap: true,
     );
   }
 }
