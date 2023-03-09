9793618e376776a858952b525549a1cf0ccddfe1
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-03-09 16:32:43 +0600

    Added bug tracking :adhesive_bandage:

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/bloc_observable.dart';
 import 'package:rick_and_morty/ui/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+    blocObserver: CharacterBlocObservable()
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,30 +18,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Rick And Morty',
       theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.black,
-        fontFamily: 'Georgia',
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white,
-          ),
-          headline2: TextStyle(
-            fontSize: 30, fontWeight: FontWeight.w700, color: Colors.white,
-          ),
-          headline3: TextStyle(
-            fontSize: 24.0, color: Colors.white,
-          ),
-          bodyText1: TextStyle(
-            fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.white,
-          ),
-          bodyText2: TextStyle(
-            fontSize: 12.0, fontWeight: FontWeight.w200, color: Colors.white,
-          ),
-          caption: TextStyle(
-            fontSize: 11.0, fontWeight: FontWeight.w100, color: Colors.grey,
-          ),
-        )
-      ),
+          brightness: Brightness.light,
+          primaryColor: Colors.black,
+          fontFamily: 'Georgia',
+          textTheme: const TextTheme(
+            headline1: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            headline2: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+            headline3: TextStyle(
+              fontSize: 24.0,
+              color: Colors.white,
+            ),
+            bodyText1: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+            bodyText2: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w200,
+              color: Colors.white,
+            ),
+            caption: TextStyle(
+              fontSize: 11.0,
+              fontWeight: FontWeight.w100,
+              color: Colors.grey,
+            ),
+          )),
       home: HomePage(
         title: 'Rick And Morty',
       ),
