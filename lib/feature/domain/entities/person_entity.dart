9d4823577055453023123b0eb7369d69026ba9fc
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-03-21 20:23:56 +0600

    Added new person entity and equatable library :art:

---
+++ lib/feature/domain/entities/person_entity.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+class PersonEntity extends Equatable {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  // Location class
+  final LocationEntity origin;
+  final LocationEntity location;
+  final String image;
+  final List<String> episode;
+  final DateTime created;
+
+  const PersonEntity({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+    required this.origin,
+    required this.location,
+    required this.image,
+    required this.episode,
+    required this.created,
+  });
+  
+  @override
+  List<Object?> get props => [id, name, status, species, type, gender, origin, location, image, episode, created];
+}
+
+// You can create another entity for location class, but i declared here
+class LocationEntity {
+  String? name;
+  String? url;
+
+  LocationEntity({this.name, this.url});
+}
