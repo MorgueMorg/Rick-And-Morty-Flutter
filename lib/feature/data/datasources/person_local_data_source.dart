cdd33a4abd11cf6d27e46dfd912bac7409ae6638
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2023-03-29 02:17:40 +0600

    Added shared_preferences library and local_data storage :test_tube:

---
+++ lib/feature/data/datasources/person_local_data_source.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:rick_and_morty/core/error/exception.dart';
+import 'package:rick_and_morty/feature/data/models/person_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class PersonLocalDataSourse {
+  /// Gets the cached [List<PersonModel>] which was gotten the last name
+  /// the user had an internet connection
+  ///
+  /// Throws [CacheException] if no cached data is present
+
+  Future<List<PersonModel>> getLastPersonsFromCache();
+  Future<void> personsToCache(List<PersonModel> persons);
+}
+
+const CACHED_PERSONS_LIST = 'CACHED_PERSONS_LIST';
+
+class PersonLocalDataSourseImpl implements PersonLocalDataSourse {
+  final SharedPreferences sharedPreferences;
+
+  PersonLocalDataSourseImpl({required this.sharedPreferences});
+
+  @override
+  Future<List<PersonModel>> getLastPersonsFromCache() {
+    final jsonPersonsList =
+        sharedPreferences.getStringList(CACHED_PERSONS_LIST);
+    if (jsonPersonsList!.isNotEmpty) {
+      print('Get Persons from Cache: ${jsonPersonsList.length}');
+      return Future.value(jsonPersonsList
+          .map((person) => PersonModel.fromJson(json.decode(person)))
+          .toList());
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> personsToCache(List<PersonModel> persons) {
+    final List<String> jsonPersonsList =
+        persons.map((person) => json.encode(person.toJson())).toList();
+
+    sharedPreferences.setStringList(CACHED_PERSONS_LIST, jsonPersonsList);
+    print('Persons to write Cache: ${jsonPersonsList.length}');
+    return Future.value(jsonPersonsList);
+  }
+}
